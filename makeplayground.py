e895aafb2cdbf720b897dd7c48eb82259b3c7024
Sean <peneyra.s@gmail.com>
2023-06-25 18:58:01 -0700

    Created a GUI
    GUI built but need to:
    - Correlate walls to the maze inputs
    - Correlate location on the map to the 'x'
    - Fix indicies on initialization
    - optimize so it doesn't run slow as shit

--- makeplayground.py
+++ makeplayground.py
@@ -1,20 +1,64 @@
-import tkinter as tk
-import time
+from tkinter import *
+from tkinter import ttk
+from runmaze import runmaze
+from time import time
 
-root = tk.Tk()
-root.geometry("300x300")
+x=100
+y=100
+x_s = 10
+y_s = 10
+
+maze = runmaze(x,y)
+
+# p = position within the maze, l = location on the local map
+p = (0,0)
+l = [5,5]
+
+# m will be the map horizontal and vertical segments
+m_h = [None]*(x_s*y_s)
+m_v = [None]*(x_s*y_s)
+
+def move(p):
+    print('hello')
+
+def frame(l):
+    ttk.Label(mf,text="").grid(column=l[0]*2+1,row=l[1]*2+1)
+    if l == [5,5]: l = [6,6]
+    elif l == [6,6]: l = [5,5]
+    ttk.Label(mf,text="x").grid(column=l[0]*2+1,row=l[1]*2+1)
+
+def build(top,left):
+    print('stuff')
+
+# begin the game loop
+root = Tk()
 root.title("   M a z e   ")
 
-t = "0"
-s = "123456789012345678901234567890"
+mf = ttk.Frame(root,padding=(5,5,5,5)).grid(row=0,column=0)
 
-pg = tk.Text(root, height = 30, width = 30)
+# Need to fix the indicies on this!!!!!!!
+for i in range(x_s):
+    for j in range(y_s):
+        m_v[i + j*x_s] = ttk.Frame(mf,height=20,width=4,borderwidth=2,relief='sunken').grid(column=2*i,row=2*j+1)
+        m_h[i + j*x_s] = ttk.Frame(mf,height=4,width=20,borderwidth=2,relief='sunken').grid(column=2*i+1,row=2*j)
+for i in range(x_s):
+    ttk.Frame(mf,height=4,width=20,borderwidth=2,relief='sunken').grid(column=2*i+1,row=2*y_s)
+for j in range(y_s):
+    ttk.Frame(mf,height=20,width=4,borderwidth=2,relief='sunken').grid(column=2*x_s,row=2*j+1)
 
-pg.pack()
-
-
-for i in s:
-    t = i
-    pg.insert(tk.END, t)
-    tk.mainloop()
-
+root.update()
+while True:
+    try:
+        t = time()
+        build(0,0)
+        root.after(10,frame(l))
+        if l == [5,5]:
+            l = [6,6]
+        elif l == [6,6]:
+            l = [5,5]
+        root.update()
+        print(time()-t)
+    except:
+        exit()
+        pass
+root.mainloop()
--- runmaze.py
+++ runmaze.py
@@ -2,43 +2,42 @@ from makemaze import makemaze
 from time import time
 from printmaze import printmaze
 
-# give dimensions of the maze
-x = 100
-y = 100
+def runmaze(x,y):
+    # give dimensions of the maze
 
-t = time()
+    t = time()
 
-# initialize the maze
-maze = makemaze(x,y)
-# set your starting spot to 0,0
-start = ('0','0')
-blank = [1,1,'']
-check = [start]
-maze[start][2] = 'empty'
+    # initialize the maze
+    maze = makemaze(x,y)
+    # set your starting spot to 0,0
+    start = ('0','0')
+    blank = [1,1,'']
+    check = [start]
+    maze[start][2] = 'empty'
 
-# check to see which spaces are achievable
-while check != []:
-    for mc in check:
-        #print(s)
-        #print(maze[s[0],s[1]])
-        # check up
-        c = [int(mc[0]),int(mc[1])]
-        [mu,mr,md,ml] = [(str(c[0]),str(c[1]-1)),(str(c[0]+1),str(c[1])),(str(c[0]),str(c[1]+1)),(str(c[0]-1),str(c[1]))]
+    # check to see which spaces are achievable
+    while check != []:
+        for mc in check:
+            #print(s)
+            #print(maze[s[0],s[1]])
+            # check up
+            c = [int(mc[0]),int(mc[1])]
+            [mu,mr,md,ml] = [(str(c[0]),str(c[1]-1)),(str(c[0]+1),str(c[1])),(str(c[0]),str(c[1]+1)),(str(c[0]-1),str(c[1]))]
 
-        if maze.get(mu,'') != '' and maze.get(mu,blank)[1] == 0 and maze.get(mu,blank)[2] != 'empty':
-            maze[mu][2] = 'empty'
-            check.append(mu)
-        if maze.get(mr,'') != '' and maze.get(mc,blank)[0] == 0 and maze.get(mr,blank)[2] != 'empty':
-            maze[mr][2] = 'empty'
-            check.append(mr)
-        if maze.get(md,'') != '' and maze.get(mc,blank)[1] == 0 and maze.get(md,blank)[2] != 'empty':
-            maze[md][2] = 'empty'
-            check.append(md)
-        if maze.get(ml,'') != '' and maze.get(ml,blank)[0] == 0 and maze.get(ml,blank)[2] != 'empty':
-            maze[ml][2] = 'empty'
-            check.append(ml)
-        check.remove(mc)
-
+            if maze.get(mu,'') != '' and maze.get(mu,blank)[1] == 0 and maze.get(mu,blank)[2] != 'empty':
+                maze[mu][2] = 'empty'
+                check.append(mu)
+            if maze.get(mr,'') != '' and maze.get(mc,blank)[0] == 0 and maze.get(mr,blank)[2] != 'empty':
+                maze[mr][2] = 'empty'
+                check.append(mr)
+            if maze.get(md,'') != '' and maze.get(mc,blank)[1] == 0 and maze.get(md,blank)[2] != 'empty':
+                maze[md][2] = 'empty'
+                check.append(md)
+            if maze.get(ml,'') != '' and maze.get(ml,blank)[0] == 0 and maze.get(ml,blank)[2] != 'empty':
+                maze[ml][2] = 'empty'
+                check.append(ml)
+            check.remove(mc)
+    return maze
 #print(spaces_good)
-print(time() - t)
-printmaze(maze,x,y)
+#print(time() - t)
+#printmaze(maze,x,y)
