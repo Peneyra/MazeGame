ea879711a9e2b39ba4922e7fd2be0f790877f152
Sean <peneyra.s@gmail.com>
2023-06-27 09:56:45 -0700

    Fixed the local maze building
    Changed the m_h and m_v arrays to be a 2D array. Fixed the mapping from the large maze to the local maze.
    
    Next step:
    Add the controls

--- makeplayground.py
+++ makeplayground.py
@@ -17,8 +17,8 @@ l = [5,5]
 tl = ('0','0')
 
 # m will be the map horizontal and vertical segments
-m_h = [None]*((x_s)*(y_s+1))
-m_v = [None]*((x_s+1)*(y_s))
+m_h = [[None for i in range(y_s+1)] for j in range(x_s)]
+m_v = [[None for i in range(y_s)] for j in range(x_s+1)]
 
 def move(p):
     pass
@@ -30,64 +30,51 @@ def frame(l):
     ttk.Label(mf,text="x").grid(column=l[0]*2+1,row=l[1]*2+1)
 
 def build(tl):
-    t = int(tl[0])
-    l = int(tl[1])
+    t = int(tl[0])-1
+    l = int(tl[1])-1
     for i in range(x_s+1):
-        for j in range(y_s):
-            if (i,j) == (0,0): continue
+        for j in range(y_s+1):
 
             # If the top and left rows end off the maze definions
             # then assign the current space a blank
-            try: s = maze[(str(l+i-1),str(t+j-1))]
+            try: s = maze[(str(l+i),str(t+j))]
             except: s = blank
 
-            if s[0] == 1:
-                m_v[i+j*y_s]['relief']='sunken'
-            else:
-                m_v[i+j*y_s]['relief']='flat'
-            if s[1] == 1:
-                print(i+j*y_s)
-            else:
-                print(i+j*y_s)
+            if j < y_s:
+                if s[0]==0 and m_v[i][j]['relief']!='flat':
+                    m_v[i][j]['relief']='flat'
+                elif s[0]==1 and m_v[i][j]['relief']!='sunken':
+                    m_v[i][j]['relief']='sunken'
+            if i < x_s:
+                if s[1]==0 and m_h[i][j]['relief']!='flat':
+                    m_h[i][j]['relief']='flat'
+                elif s[1]==1 and m_h[i][j]['relief']!='sunken':
+                    m_h[i][j]['relief']='sunken'
 
-#def build(j):
-#    print(len(m_v),len(m_h))
-#    for i in range(len(m_v)):
-#        m_v[i]['relief']='flat'
-#    for i in range(len(m_h)):
-#        m_h[i]['relief']='flat'
-#
-# begin the game loop
+# -----------------------------------------------------------------------
+# begin building the GUI
 root = Tk()
 root.title("   M a z e   ")
 
-mf = ttk.Frame(root,padding=(5,5,5,5)).grid(row=0,column=0)
+# build a frame to put all the walls into
+mf = ttk.Frame(root,padding=5)
+mf.grid(row=0,column=0)
 
-# initiate the maze and map each wall to a different index in m_v and m_h
-# converting k to vertical: [x,y]=[k%(x_s+1),(k-j_v)/(x_s+1)]
-i_v, j_v, i_h, j_h = 0, 0, 0, 0
-for k in range((x_s+1)*(y_s+1)):
-    if j_v < y_s: 
-        m_v[k] = ttk.Frame(mf,height=20,width=4,borderwidth=2,relief='sunken')
-        m_v[k].grid(column=2*i_v,row=1+2*j_v)
-        print(k)
-    if i_h < x_s: 
-        m_h[k] = ttk.Frame(mf,height=4,width=20,borderwidth=2,relief='sunken')
-        m_h[k].grid(column=1+2*i_h,row=2*j_h)
-    i_v, j_h = (i_v+1)%(x_s+1), (j_h+1)%(y_s+1)
-    if i_v == 0: j_v += 1
-    if j_h == 0: i_h += 1
+# map each wall to vertical m_v and horizontal m_h walls
+for i in range(x_s+1):
+    for j in range(y_s+1):
+        if j < y_s:
+            m_v[i][j] = ttk.Frame(mf,height=20,width=4,borderwidth=2,relief='sunken')
+            m_v[i][j].grid(column=2*i,row=1+2*j)
+        if i < x_s:
+            m_h[i][j] = ttk.Frame(mf,height=4,width=20,borderwidth=2,relief='sunken')
+            m_h[i][j].grid(column=1+2*i,row=2*j)
 
-root.update()
 while True:
     try:
         t = time()
-        build(('0','0'))
+        build(tl)
         root.after(100,frame(l))
-        if l == [5,5]:
-            l = [6,6]
-        elif l == [6,6]:
-            l = [5,5]
         root.update()
         print(time()-t)
     except:
