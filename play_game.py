ce7d00d2a26051ca2d383bb8618cab216efda2b2
Sean <peneyra.s@gmail.com>
2023-07-04 20:41:33 -0700

    Functioning Game
    - changed how the goal is set into the maze by trying the center square, then spiraling out until it finds a viable spot. will not accept a goal more than 10 spots from the center.
    - got rid of the running tally of all achievable spots in the maze

--- play_game.py
+++ play_game.py
@@ -7,8 +7,8 @@ import keyboard
 
 # x and y are dimensions of the overall maze, x_s and y_s are 
 # dimensions of the local map
-x=30
-y=30
+x=100
+y=100
 x_s = 11
 y_s = 11
 x_2 = floor(x_s/2)
@@ -17,6 +17,7 @@ y_2 = floor(y_s/2)
 # Build out the x by y maze and define a blank space for any
 # functions which need to use a space outside the bounds of the maze
 maze, goal = runmaze(x,y)
+while abs(goal[0]-floor(x/2))>10 or abs(goal[1]-floor(y/2))>10: maze, goal = runmaze(x,y)
 blank = [0,0,'empty']
 
 # p = position within the maze, ps = location on the local map
@@ -38,10 +39,10 @@ t['up'] = t.get('up',0)
 
 def checkmove(a):
     if a == 'zzz': return False
-    if a == 'a': dp, i = (p[0]-1,p[1]), 0
-    if a == 's': dp, i = (p[0],p[1]), 1
-    if a == 'd': dp, i = (p[0],p[1]), 0
-    if a == 'w': dp, i = (p[0],p[1]-1), 1
+    elif a == 'a': dp, i = (p[0]-1,p[1]), 0
+    elif a == 's': dp, i = (p[0],p[1]), 1
+    elif a == 'd': dp, i = (p[0],p[1]), 0
+    elif a == 'w': dp, i = (p[0],p[1]-1), 1
     try: 
         if maze[dp][i] == 1: return False
     except: pass
@@ -51,21 +52,25 @@ def build(p):
     i_0 = min(max(p[0]-x_2,0),x-x_s)-1
     j_0 = min(max(p[1]-y_2,0),y-y_s)-1
 
-    try: p_g.destroy()
-    except: pass
-
+    p_g['background']='white'
     for i in range(x_s+1):
         for j in range(y_s+1):
             # If the top and left rows end off the maze definions
             # then assign the current space a blank
-            if i_0+i >= 0 and i_0+i <= x-2 and j_0+j>=0 and j_0+j <= y-2:
-                s = maze[(i_0+i,j_0+j)]
-            else: 
-                s = blank
+#            if i_0+i >= 0 and i_0+i <= x-2 and j_0+j>=0 and j_0+j <= y-2:
+#                s = maze[(i_0+i,j_0+j)]
+#            else: 
+#                s = blank
 
-#            if (i_0+i,j_0+j) == goal:
-#                p_g = Frame(mf,height=20,width=20,background='blue')
-#                p_g.grid(column=(i_0+i)*2+1,row=(j_0+j)*2+1)
+            try: s = maze[(i_0+i,j_0+j)]
+            except: s = blank
+
+            if s[2] == 'goal':
+                try: 
+                    p_g.grid(column=i*2-1,row=j*2-1)
+                    p_g['background']='blue'
+                except:
+                    p_g['background']='white'
 
             if s[0]==0: w_v[i][j-1]['relief']='flat'
             elif s[0]==1: w_v[i][j-1]['relief']='sunken'
@@ -74,9 +79,8 @@ def build(p):
             elif s[1]==1: w_h[i-1][j]['relief']='sunken'
 
     # check to see if the position on the local map moved
-    p_t = [min(p[0],max(p[0]+x_s-x,x_2)),
-           min(p[1],max(p[1]+y_s-y,y_2))]
-    # if the position on the map has moved, update the x
+    p_t = [min(p[0],max(p[0]+x_s-x,x_2)),min(p[1],max(p[1]+y_s-y,y_2))]
+    # if the position on the map has moved, update the character posit
     p_x.grid(column=p_t[0]*2+1,row=p_t[1]*2+1)
 
 def frame_update(p):
@@ -96,23 +100,25 @@ root = Tk()
 root.title("   M a z e   ")
 
 # build a frame to put all the walls into
-mf = ttk.Frame(root,padding=5)
+mf = Frame(root,background='white')
 mf.grid(row=0,column=0)
 
 # define a blank position and a character position. Every time
 # the char moves, p_x will be placed and the old position will
 # be replaced with p_b
-p_x = Frame(mf,height=20,width=20,background='green')
+p_g = Frame(mf,height=10,width=10,background='white')
+p_g.grid(column=p_s[0]*2+1,row=p_s[1]*2+1)
+p_x = Frame(mf,height=10,width=10,background='green')
 p_x.grid(column=p_s[0]*2+1,row=p_s[1]*2+1)
 
 # map each wall to vertical m_v and horizontal m_h walls
 for i in range(x_s+1):
     for j in range(y_s+1):
         if j < y_s:
-            w_v[i][j] = ttk.Frame(mf,height=20,width=4,borderwidth=2,relief='sunken')
+            w_v[i][j] = Frame(mf,height=20,width=4,borderwidth=2,relief='sunken',background='white')
             w_v[i][j].grid(column=2*i,row=1+2*j)
         if i < x_s:
-            w_h[i][j] = ttk.Frame(mf,height=4,width=20,borderwidth=2,relief='sunken')
+            w_h[i][j] = Frame(mf,height=4,width=20,borderwidth=2,relief='sunken',background='white')
             w_h[i][j].grid(column=1+2*i,row=2*j)
 
 frame_update(p)
--- runmaze.py
+++ runmaze.py
@@ -9,7 +9,6 @@ def runmaze(x,y):
     start = (0,0)
     blank = [1,1,'']
     check = [start]
-    check_all = [start]
     maze[start][2] = 'empty'
 
     # check to see which spaces are achievable
@@ -25,10 +24,21 @@ def runmaze(x,y):
                 if maze.get(ml[0],'') != '' and maze.get(ml[0],blank)[ml[1]] == 0 and maze.get(ml[0],blank)[2] != 'empty':
                     maze[ml[0]][2] = 'empty'
                     check.append(ml[0])
-                    check_all.append(ml[0])
             check.remove(mc)
 
-    check_all.sort()
-    goal = check_all[floor(len(check_all)/2)]
-    maze[goal][2] = 'goal'
-    return maze, goal
+    # place the goal near the center
+    c = [floor(x/2),floor(y/2)]
+    k1=1
+    k2=1
+    while k1<min(floor(x/2),floor(y/2)):
+        for i in range(2):
+            for j in range(k1):
+                if maze[(c[0],c[1])][2]=='empty':
+                        maze[(c[0],c[1])][2] = 'goal'
+                        print(maze[(c[0],c[1])])
+                        print((c[0],c[1]))
+                        return maze, (c[0],c[1])
+                c[i] += k2
+        k1+=1
+        k2=k2*(-1)
+    return maze, (c[0],c[1])
